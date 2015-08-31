b6da82e34a9c808701e216b6f4540ab0d7c6a1fb
Knutakir <knutkirk@hotmail.com>
2015-08-31 16:04:16 +0200

    Øving 1
    Måtte legge til alle filene på nytt med lettere leselige navn.

--- Øving 1/Oppgave1.java
+++ Øving 1/Oppgave1.java
@@ -1,24 +1,19 @@
-/*Ã˜ving 1*/
+/*Øving 1*/
 /*Oppgave 2.13.1 side 83/
 
-<<<<<<< HEAD:Ã˜ving 1/Oppgave1.java
 /*importere bibloteket så man slipper å skrive så mye lengre nedi*/
-=======
-
-/*importere bibloteket sÃ¥ man slipper Ã¥ skrive sÃ¥ mye lengre nedi*/
->>>>>>> origin/Ã˜vinger:Ã˜ving 1/Oppgave_2-13-1.java
 import static javax.swing.JOptionPane.*;
 class Oppgave1{
 	public static void main(String[] args){
-		/*Brukeren av programmet skrive inn kor mange tommer som skal gjÃ¸res om*/
+		/*Brukeren av programmet skrive inn kor mange tommer som skal gjøres om*/
 		String tommerLest = showInputDialog("Tommer: ");
 
 		/*Lese kor mange tommer*/
 		double tommer = Double.parseDouble(tommerLest);
 
-		/*Sjekke om tommmer er stÃ¸rre enn 0*/
+		/*Sjekke om tommmer er større enn 0*/
 		if(tommer > 0){
-			/*gjÃ¸re dette om til cm*/
+			/*gjøre dette om til cm*/
 			double centimeter = tommer * 2.54;
 
 			/*Vise kor mange tommer og cm i meldingsboks*/
@@ -28,21 +23,14 @@ class Oppgave1{
 				showMessageDialog(null, tommer + " tommer = " + centimeter + " centimeter.");
 			}
 		} else {
-			showMessageDialog(null, "Du mÃ¥ skrive ett tall stÃ¸rre enn null.");
+			showMessageDialog(null, "Du må skrive ett tall større enn null.");
 		}
 	}
 }
 
 /* Testdatasett
 nr1:		tall:	resultat:
-<<<<<<< HEAD:Ã˜ving 1/Oppgave1.java
 1			0		Du må skrive ett tall større enn null.
 2			100		100.0 tommer = 254.0 centimeter.
 3			2		2.0 tommer = 5.08 centimeter.
 4			-2		Du må skrive ett tall større enn null.*/
-=======
-1		0		Du mÃ¥ skrive ett tall stÃ¸rre enn null.
-2		100		100.0 tommer = 254.0 centimeter.
-3		2		2.0 tommer = 5.08 centimeter.
-4		-2		Du mÃ¥ skrive ett tall stÃ¸rre enn null.*/
->>>>>>> origin/Ã˜vinger:Ã˜ving 1/Oppgave_2-13-1.java
