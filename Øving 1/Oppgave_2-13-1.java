c69c337301942d440310efd441ef758b777e1ebf
Knut Kirkhorn <knutkirk@hotmail.com>
2015-08-31 15:35:31 +0200

    Update Oppgave_2-13-1.java

--- Øving 1/Oppgave_2-13-1.java
+++ Øving 1/Oppgave_2-13-1.java
@@ -1,20 +1,20 @@
-/*Øving 1*/
+/*Ã˜ving 1*/
 /*Oppgave 2.13.1 side 83/
 
 
-/*importere bibloteket så man slipper å skrive så mye lengre nedi*/
+/*importere bibloteket sÃ¥ man slipper Ã¥ skrive sÃ¥ mye lengre nedi*/
 import static javax.swing.JOptionPane.*;
 class Oppgave_2-13-1{
 	public static void main(String[] args){
-		/*Brukeren av programmet skrive inn kor mange tommer som skal gjøres om*/
+		/*Brukeren av programmet skrive inn kor mange tommer som skal gjÃ¸res om*/
 		String tommerLest = showInputDialog("Tommer: ");
 
 		/*Lese kor mange tommer*/
 		double tommer = Double.parseDouble(tommerLest);
 
-		/*Sjekke om tommmer er større enn 0*/
+		/*Sjekke om tommmer er stÃ¸rre enn 0*/
 		if(tommer > 0){
-			/*gjøre dette om til cm*/
+			/*gjÃ¸re dette om til cm*/
 			double centimeter = tommer * 2.54;
 
 			/*Vise kor mange tommer og cm i meldingsboks*/
@@ -24,15 +24,14 @@ class Oppgave_2-13-1{
 				showMessageDialog(null, tommer + " tommer = " + centimeter + " centimeter.");
 			}
 		} else {
-			showMessageDialog(null, "Du må skrive ett tall større enn null.");
+			showMessageDialog(null, "Du mÃ¥ skrive ett tall stÃ¸rre enn null.");
 		}
 	}
 }
 
 /* Testdatasett
 nr1:		tall:	resultat:
-1			0		Du må skrive ett tall større enn null.
-2			100		100.0 tommer = 254.0 centimeter.
-3			2		2.0 tommer = 5.08 centimeter.
-4			-2		Du må skrive ett tall større enn null.
-*/
+1		0		Du mÃ¥ skrive ett tall stÃ¸rre enn null.
+2		100		100.0 tommer = 254.0 centimeter.
+3		2		2.0 tommer = 5.08 centimeter.
+4		-2		Du mÃ¥ skrive ett tall stÃ¸rre enn null.*/
