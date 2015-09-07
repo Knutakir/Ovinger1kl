658cf1aefe3d26cac03236793b1e08026ff85632
Knut Kirkhorn <knutkirk@hotmail.com>
2015-09-07 18:37:16 +0200

    Update Spiller.java

--- Øving 4/Oppgave 3/Spiller.java
+++ Øving 4/Oppgave 3/Spiller.java
@@ -4,7 +4,6 @@ class Spiller{
 	private String navn;
 
 	//public Spiller(){
-
 	//}
 
 	public int getSumPoeng(){
@@ -12,19 +11,18 @@ class Spiller{
 	}
 
 	public void kastTerningen(){
-		int terningkast = terning.nextInt(6) + 1; //Kaster terningen. Og fÂr et tall mellom 1 og 6. Man mÂ legge til 1 fordi dette gir tall i intervallet: [0, 5]
+		int terningkast = terning.nextInt(6) + 1; //Kaster terningen. Og f√•r et tall mellom 1 og 6. Man m√• legge til 1 fordi dette gir tall i intervallet: [0, 5]
 		if(terningkast == 1){ //Sjekker om terningen som blir "kastet" blir 1.
 			sumPoeng = 0; //Setter poengen tilbake til 0 dersom den er det.
 		} else {
 			sumPoeng += terningkast;
 		}
 
-		if(sumPoeng > 100){ //Endring som stod pÂ slutten av oppgaven ("En raffinering av oppgaven").
+		if(sumPoeng > 100){ //Endring som stod p√• slutten av oppgaven ("En raffinering av oppgaven").
 			sumPoeng -= terningkast;
 		}
 	}
 
 	//public void erFerdig(){ //WASTE
-
 	//}
 }
