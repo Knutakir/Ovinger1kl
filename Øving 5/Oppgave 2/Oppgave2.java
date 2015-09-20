11abea556959cf7fdbdeff37030a29f012052ec8
Knutakir <knutkirk@hotmail.com>
2015-09-20 23:12:50 +0200

    Endring
    Mer brukervennelig programm.

--- Øving 5/Oppgave 2/Oppgave2.java
+++ Øving 5/Oppgave 2/Oppgave2.java
@@ -1,17 +1,40 @@
+import static javax.swing.JOptionPane.*;
+
 class Oppgave2{
 	public static void main(String[] args){
-		BrokRegner brok1 = new BrokRegner(9, 3);
 
-		brok1.subraksjon(1, 4);
-		System.out.println(brok1.getTeller() + "/" + brok1.getNevner());
-		brok1.divisjon(4, 2);
-		System.out.println(brok1.getTeller() + "/" + brok1.getNevner());
-		brok1.multiplikasjon(7, 2);
-		System.out.println(brok1.getTeller() + "/" + brok1.getNevner());
-		brok1.addisjon(4, 8);
-		System.out.println(brok1.getTeller() + "/" + brok1.getNevner());
+		String teller = showInputDialog("Skriv inn teller: ");
+		int tellerLest = Integer.parseInt(teller);
+		String nevner = showInputDialog("Skriv inn nevner: ");
+		int nevnerLest = Integer.parseInt(nevner);
 
-		brok1.forkortBrok();
-		System.out.println(brok1.getTeller() + "/" + brok1.getNevner());
+		BrokRegner brok1 = new BrokRegner(tellerLest, nevnerLest);
+
+		while(true){
+			String operasjon = showInputDialog("Hvilken operasjon vi du utføre? (SUB=1, DIV=2, MUL=3, ADD=4)");
+			int opLest = Integer.parseInt(operasjon);
+
+			String nyTeller = showInputDialog("Skriv inn en ny teller: ");
+			int nyTellerLest = Integer.parseInt(nyTeller);
+			String nyNevner = showInputDialog("Skriv inn en ny nevner: ");
+			int nyNevnerLest = Integer.parseInt(nyNevner);
+
+			switch(opLest){
+				case 1:
+					brok1.subraksjon(nyTellerLest, nyNevnerLest);
+					break;
+				case 2:
+					brok1.divisjon(nyTellerLest, nyNevnerLest);
+					break;
+				case 3:
+					brok1.multiplikasjon(nyTellerLest, nyNevnerLest);
+					break;
+				case 4:
+					brok1.addisjon(nyTellerLest, nyNevnerLest);
+					break;
+				}
+				brok1.forkortBrok();
+				showMessageDialog(null, "Den nye brøken er: " + brok1.getTeller() + "/" + brok1.getNevner());
+		}
 	}
 }
